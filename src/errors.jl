54aa0ea1cffeca45da368709d959449bf2361e83
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-09 12:02:52 +0000

    Snapshot of branch master taken on 2019-10-09T08:02:50.594-04:00 from "https://github.com/UnofficialJuliaMirror/GeoStatsBase.jl-323cb8eb-fbf6-51c0-afd0-f8fba70507b2"

--- src/errors.jl
+++ src/errors.jl
@@ -10,18 +10,13 @@ A method for estimating error of geostatistical solvers.
 abstract type AbstractErrorEstimator end
 
 """
-    estimate_error(solver, problem, eestimator)
+    error(solver, problem, eestimator)
 
 Estimate error of `solver` in a given `problem` with
 `eestimator` error estimation method.
 """
-estimate_error(::AbstractSolver, ::AbstractProblem,
-               ::AbstractErrorEstimator) = @error "not implemented"
-
-compare(solvers::AbstractVector{S}, problem::P,
-        eestimator::AbstractErrorEstimator) where {S<:AbstractSolver,
-                                                   P<:AbstractProblem} =
-  [estimate_error(solver, problem, eestimator) for solver in solvers]
+Base.error(::AbstractSolver, ::AbstractProblem,
+           ::AbstractErrorEstimator) = @error "not implemented"
 
 #------------------
 # IMPLEMENTATIONS
